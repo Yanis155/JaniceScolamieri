2b3a936eb2ad13c347b4722df7a193ec08b889d8
Yanis155 <102390284+Yanis155@users.noreply.github.com>
2022-04-10 20:50:05 -0300

    Add files via upload

---
+++ trabajos2/js/scrips/funciones.js
@@ -0,0 +1,15 @@
+function hola(){
+    alert ("hola");
+}
+
+function chau(){
+  alert ("chau");
+}
+   
+function pasar(){
+ alert ("pasar");
+}
+
+function cambiar(){
+    document.getElementById("a1").style.backgroundColor="red";
+}
