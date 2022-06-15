2529c95adb5e912ba1841aebbfbaf0f37a40adaf
Yanis155 <102390284+Yanis155@users.noreply.github.com>
2022-06-14 23:27:00 -0300

    "trabajo 2"

---
+++ trabajo2/js/script/function.js
@@ -0,0 +1,15 @@
+function  hola () {
+  alert ("hola");
+}
+
+function chau(){
+  alert ("chau");
+}
+   
+function pasar(){
+ alert ("pasar");
+}
+
+function cambiar_color() {
+  document.getElementById("A1").style.backgroundColor = "Pink"
+}
