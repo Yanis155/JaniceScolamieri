3a0d0702d41a8702dfe8321f123364327a36f2f0
JaniceScolamieri <102390284+Yanis155@users.noreply.github.com>
2022-10-05 21:22:09 -0300

    traits

--- traits/ventas.php
+++ traits/ventas.php
@@ -4,12 +4,20 @@ require_once("Tienda.php");
 
 $objProducto= new Tienda();  //puedo utilizar todos los metodos de prod, tienda y carrito en objProducto (reutilizo codigo)
 
-$objProducto-> setProducto("Alfajor",250,100);
+$strProducto= "Teclado";  //las variables ayudan a que si tuviera una bd estas var solo tendria que cambiar su valor y ayuda a reutilizar cod
+$fltPrecio= 115;        //si tuviera otro precio o se actualiza por otro, la var fltP precio solo cambia el 115 por otro valor
+$intStock= 10;          //lo mismo pasa con el stock o cantidad vendida.
+$intCantidadVendida= 3;
 
+
+$objProducto-> setProducto($strProducto,$fltPrecio,$intStock);
 echo $objProducto-> getProducto();
+
 echo "<br>";
-$objProducto-> setCarrito("Alfajor",5);
-echo $objProducto-> getProducto();
+$objProducto-> setCarrito($strProducto,$intCantidadVendida);
+$objProducto-> setStock($intCantidadVendida);
 
+echo $objProducto-> getProducto();
+echo $objProducto-> getCarrito();
 
 ?>
